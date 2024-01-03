170de70a2a602a54abec4ef2556d810fdf1a984e
Kevin Genus <kevin@kevingenus.com>
2024-01-03 18:27:27 -0500

    Adds Post

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,15 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Dorthy", email: "dorthy@thewiz.com"},
+      %{id: 2, name: "ScareCrow", email: "sc@thewiz.com"},
+      %{id: 3, name: "Tin-Man", email: "tin.man@thewiz.com"},
+      %{id: 4, name: "Lion", email: "thebig_l@thewiz.com"}
+    ]
+
+    json(conn, %{users: users})
+  end
 end
